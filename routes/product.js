109dfd2c4de4a943673d6b29bdee7c5773d988ce
KevadiyaDarshil <kevadiyadarshil13@gmail.com>
2025-04-11 17:15:47 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -19,6 +19,8 @@ const upload = multer({ storage: storage })
 router.get('/', PC.viewprod)
 router.post('/addproduct', auth.AuthSecure, upload.single('image'), PC.addprod)
 router.delete('/delteproduct/:id', auth.AuthSecure, PC.deleteprod)
+router.get('/find/', SC.search)
+router.get('/find/cat/:catid', SC.catsearch)
 router.patch('/updateproduct/:id',upload.single('image'), PC.updateprod)
 
 module.exports = router;
