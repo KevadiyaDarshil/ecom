ce7096b8d2ac77b33fe045722626e0b26ce8e38f
KevadiyaDarshil <kevadiyadarshil13@gmail.com>
2025-04-12 10:42:40 +0530

    Update prodmodel.js

--- model/prodmodel.js
+++ model/prodmodel.js
@@ -17,4 +17,5 @@ var prodSchema = new mongoose.Schema({
    offer: Number,
     price: Number
 })
+prodSchema.index({ prodname: 'text', description: 'text', cat_name: 'text' });
 module.exports = mongoose.model('product', prodSchema)
